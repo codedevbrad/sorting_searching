e7749d69e74188e6e42e69e23083c862f153a608
brad lumber <46296577+codedevbrad@users.noreply.github.com>
2020-01-17 20:00:36 +0000

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,21 +1,22 @@
 function bubbleSort( array ) {
-	for ( var i = 0; i < array.length; i++ ) {
-    let didSwap = false;
-		for ( var j = 0; j < array.length - i - 1; j++ ) {
+    for ( var i = 0; i < array.length; i++ ) {
+        let didSwap = false;
+	for ( var j = 0; j < array.length - i - 1; j++ ) {
+		
     		let a = array[j];
-        let b = array[j+1];
+     	        let b = array[j+1];
          
-        if ( a > b ) {
-        		swap( array , j , j+1 );
-            didSwap = true;
-        }
-       //  console.log( j , j+1 );
-        if ( j+1 == array.length - 1 && didSwap == false ) {
-           return array; 
+		if ( a > b ) {
+		    swap( array , j , j+1 );
+		    didSwap = true;
+		}
+	     
+		if ( j+1 == array.length - 1 && didSwap == false ) {
+		   return array; 
+		}
         }
     }
-  }
-	return array;  
+    return array;  
 }
 
 let sorted = bubbleSort( array );
