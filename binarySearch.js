9192c234cba90d9071fbd1b5be290ed37d88a079
brad lumber <46296577+codedevbrad@users.noreply.github.com>
2020-01-17 19:56:33 +0000

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -1,4 +1,4 @@
-```javascript
+
 var items = "Aaron Adam Aiden Alex Alexander Alfie Archie Arthur Ashton Bailey Ben Bejamin Blake Bobby Caleb Callum Cameron Charles Charlie Connor Daniel David Dexter Dylan Edward Elijah Elliot Ethan Evan Finlay Finley Frankie Freddie Frederick Gabriel George Harley Harrison Harry Harvey Hayden Henry Isaac Jack Jackson Jacob Jake James Jamie Jayden Jenson Joel John Joseph Joshua Jude Kai Kayden Kian Kyle Leo Leon Lewis Liam Logan Louie Louis Luca Lucas Luke Mason Matthew Max MichaelMohammad Mohammed Muhammad Nathan Noah Oliver Ollie Oscar Owen Reece Reuben Rhys Riley Robert Ryan Samuel Sebastian Stanley Taylor Theo Thomas Toby Tommy Tyler William Zachary";
 
 items = items.split(" ");
@@ -27,4 +27,4 @@ function binary( remaining , target ) {
         }
     }  
 }
-```
+
